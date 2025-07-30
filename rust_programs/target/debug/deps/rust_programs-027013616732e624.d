22977ad94a62fbf7149fb00175a80756c0ab9a56
daviegito <daviegitocoelho@gmail.com>
2025-07-30 11:56:03 -0300

    feat: added simple calculator program. Chore: finish its tests

---
+++ rust_programs/target/debug/deps/rust_programs-027013616732e624.d
@@ -0,0 +1,5 @@
+/home/daviegito/git/testing/rust_programs/target/debug/deps/rust_programs-027013616732e624.d: src/main.rs
+
+/home/daviegito/git/testing/rust_programs/target/debug/deps/librust_programs-027013616732e624.rmeta: src/main.rs
+
+src/main.rs:
---
+++ rust_programs/target/debug/deps/rust_programs-2474f4f3aaac1b05.d
@@ -0,0 +1,5 @@
+/home/daviegito/git/testing/rust_programs/target/debug/deps/rust_programs-2474f4f3aaac1b05.d: src/main.rs
+
+/home/daviegito/git/testing/rust_programs/target/debug/deps/librust_programs-2474f4f3aaac1b05.rmeta: src/main.rs
+
+src/main.rs:
