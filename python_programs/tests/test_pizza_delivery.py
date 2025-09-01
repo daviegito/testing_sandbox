2f23c5a049b365f628f4b600633dfc246984adf6
daviegito <daviegitocoelho@gmail.com>
2025-08-31 21:46:47 -0300

    feat: adding documentation through Docusaurus. Chore: modify the template

---
+++ python_programs/tests/test_pizza_delivery.py
@@ -0,0 +1,18 @@
+'''
+from programs.pizza_delivery import Pizza, Order, User 
+
+import pytest
+
+def test_if_order_correct():
+  pass
+
+//make it only accept int as to not have too much headache in the future
+def test_if_floats_are_allowed_for_order():
+  pass
+    
+def test_if_username_accepts_int():
+  pass
+  
+def test_
+  
+'''
