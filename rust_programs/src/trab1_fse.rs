9eb5c0000cff76887e5b2b35bd42cef69737e9b1
daviegito <daviegitocoelho@gmail.com>
2026-04-20 15:46:12 -0300

    feat: adição de início pro trabalho 1 de FSE

---
+++ rust_programs/src/trab1_fse.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+    //Entrega 1
+    //Módulo capaz de controlar a GPIO com Inputs (por polling e interrupção) / Outputs
+    //(Liga/Desliga e PWM).
+    //
+    //Servidores distribuídos
+    //1. Controlar os semáforos via GPIO com temporização
+    //2. Controlar os botões de pedestre via GPIO
+    //3. Medir a velocidade dos veículos via GPIO 
+    //4. Reportar ao Servidor Central via TCP/IP a cada 2 segundos a contagem de veículos por sensor
+    //5. Caso o veículo esteja acima de 60 km/h, enviar push ao Servidor com {cruzamento,
+    //   sensor_id, velocidade_kmh, timestamp}
+    //6. 
+
+/*
+Estruturas que usaremos pra representar o que tá no enunciado
+
+struct semaforo {
+  
+}
+
+struct carro {
+
+}
+
+*/
+
+fn main() {
+
+}
